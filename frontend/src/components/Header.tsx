f3f9e1fc371540955dc52049a089f2bd4c4f8eb8
Sylvia Yung <sylvia.yung@ntnu.no>
2025-02-17 14:39:07 +0100

    Refactor(#26) Change header dropdown and add button
    Changed the header dropdown for smaller and more professional size,
    moved the add "+" button closer to usericon.
    Co-authored-by: Nicolay Emil Fremstad Løvlie <Nelovlie@stud.ntnu.no>

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Logo from "../assets/Logo/NotaFlowLogo.svg";
 import Icon from "../assets/Icon/UserIcon.svg";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AuthLogout } from "./logout";
 import Button from '@mui/material/Button';
 import { useAuth } from "../firebase/func/useAuth";
@@ -18,13 +18,13 @@ const Dropdown = ({
   if (!isOpen) return null;
 
   return (
-    <div className="absolute right-0 mt-8 w-60 bg-white border-gray-300 shadow-md rounded-lg">
-      <ul className="text-gray-800 text-4xl">
-        <li className="px-4 py-2 hover:bg-indigo-100 cursor-pointer rounded-lg h-20 flex items-center justify-center" onClick={() => navigate('/user')}>
+    <div className="absolute right-0 mt-8 w-40 bg-white border-gray-300 shadow-md rounded-lg">
+      <ul className="text-gray-800 text-2xl">
+        <li className="px-4 py-2 hover:bg-indigo-100 cursor-pointer rounded-lg h-12 flex items-center justify-center" onClick={() => navigate('/user')}>
           Min konto
         </li>
         <li
-          className="px-4 py-2 hover:bg-indigo-100 cursor-pointer rounded-lg h-20 flex items-center justify-center"
+          className="px-4 py-2 hover:bg-indigo-100 cursor-pointer rounded-lg h-12 flex items-center justify-center"
           onClick={onLogout}>
           Logg ut
         </li>
@@ -43,6 +43,10 @@ export const Header = () => {
     navigate("/");
   };
 
+  useEffect(() => {
+    setIsOpen(false);
+  },[isLoggedIn]);
+
   return (
     <div className="relative border-b-[1.5px] h-23 border-b-indigo-400 bg-indigo-500 flex items-center justify-center">
         <div className="text-5xl text-white cursor-pointer" onClick={() => navigate('/Dashboard')}>
@@ -52,17 +56,6 @@ export const Header = () => {
         <div className='absolute left-10' onClick={() => navigate('/Dashboard')}>
             <img src={Logo} alt="Logo" className="h-15 w-15 cursor-pointer" />
         </div>
-      {/* Clickable plus button */}
-      {isLoggedIn && (
-        <div className="absolute right-60 z-20">
-          <button
-            className="h-8 w-8 border-2 border-black text-black bg-transparent text-3xl rounded-lg flex items-center justify-center hover:bg-indigo-900 transition cursor-pointer"
-            onClick={() => navigate('/PublishingPage')}
-          >
-            +
-          </button>
-        </div>
-      )}
                 
       {/* Log in button */}
       <div className='absolute flex items-center right-10'>
@@ -71,13 +64,23 @@ export const Header = () => {
             <Button sx={{marginRight: '20px'}} variant="contained" color='success' size='large' onClick={() => navigate('/Login')}>Logg inn</Button>
           </div>
         ) : (
-          <div
-              onMouseEnter={() => setIsOpen(true)}
-              onMouseLeave={() => setIsOpen(false)}
-              style={{ zIndex: 1000 }}
-          >
-            <img src={Icon} alt="user icon" className='h-12 w-12 cursor-pointer' />
-            <Dropdown isOpen={isOpen} navigate={navigate} onLogout={handleLogout} />
+          // Postbutton and user icon
+          <div className="right-10 flex items-center gap-6">
+            <button
+              className="h-10 w-10 border-2 border-black text-black bg-transparent text-3xl rounded-lg flex items-center justify-center hover:bg-indigo-900 transition cursor-pointer"
+              onClick={() => navigate('/PublishingPage')}
+            >
+              +
+            </button>
+            <div
+                onMouseEnter={() => setIsOpen(true)}
+                onMouseLeave={() => setIsOpen(false)}
+                style={{ zIndex: 1000 }}
+                className="relative"
+                >
+              <img src={Icon} alt="user icon" className='h-12 w-12 cursor-pointer' />
+              <Dropdown isOpen={isOpen} navigate={navigate} onLogout={handleLogout} />
+            </div>
           </div>
         )}
       </div>
