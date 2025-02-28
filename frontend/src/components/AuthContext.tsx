167ad852b4a193cc3f08dce0cce42c2b0b140e22
nicolovli <nelovlie@stud.ntnu.no>
2025-02-28 18:20:09 +0000

    fix(#43) Fixed bug
    Co-authored-by: Mari Gjul <marigju@stud.ntnu.no>

--- frontend/src/components/AuthContext.tsx
+++ frontend/src/components/AuthContext.tsx
@@ -10,21 +10,28 @@ import { auth } from "../Config/firebase-config";
 interface AuthContextType {
   user: User | null;
   isLoggedIn: boolean;
+  isLoading: boolean;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
+      setIsLoading(false);
     });
+
     return () => unsubscribe();
   }, []);
+
   return (
-    <AuthContext.Provider value={{ user, isLoggedIn: !!user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, isLoggedIn: !!user, isLoading }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
